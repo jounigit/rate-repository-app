f6049ee68fc4fa664da9093d7fed62660dc11918
Jouni Airaksinen <airaksinen.jouni@gmail.com>
2021-04-06 08:45:44 +0300

    osa 10.7

--- src/components/AppBar.jsx
+++ src/components/AppBar.jsx
@@ -1,23 +1,29 @@
 import React from 'react';
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, ScrollView } from 'react-native';
 import AppBarTab from './AppBarTab'
 import AppBarTabSignIn from './AppBarTabSignIn'
 import theme from '../theme';
 
 const styles = StyleSheet.create({
   container: {
+    display: 'flex',
+    flexDirection: "row",
     backgroundColor: theme.colors.background,
     padding: 20
-  },
-  // ...
+  }
 });
 
 const AppBar = () => {
-  return <View style={styles.container}>
-
-    <AppBarTab />
-    <AppBarTabSignIn />
-  </View>;
+  return (
+  <View style={styles.container}>
+    <ScrollView horizontal>
+        <View>
+            <AppBarTab />
+            <AppBarTabSignIn />
+        </View>
+    </ScrollView>
+   </View>
+  );
 };
 
 export default AppBar;
