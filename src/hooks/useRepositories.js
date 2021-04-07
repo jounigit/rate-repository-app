942eb782016c66d03b956458f76c617aa01ddc14
Jouni Airaksinen <airaksinen.jouni@gmail.com>
2021-04-07 20:55:42 +0300

    osa 10.11

---
+++ src/hooks/useRepositories.js
@@ -0,0 +1,22 @@
+import { useState, useEffect } from 'react';
+import { useQuery } from '@apollo/client';
+
+import { GET_REPOSITORIES } from '../graphql/queries';
+
+const useRepositories = () => {
+  const { data, error, loading, refetch } = useQuery(GET_REPOSITORIES, {
+    fetchPolicy: 'cache-and-network',
+  });
+  let repositories;
+  console.log('## REP query:: -loading: ', loading, ' -data: ', data)
+  //if (loading) return null;
+    if (error) return `Error! ${error}`;
+
+    if (data) {
+        repositories = data.repositories
+    }
+
+  return { repositories, loading, refetch };
+};
+
+export default useRepositories;
