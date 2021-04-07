598af4e1948a153fa5a7e4499221d908c43182d9
Jouni Airaksinen <airaksinen.jouni@gmail.com>
2021-04-07 21:58:34 +0300

    osa 10.12

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { NativeRouter } from 'react-router-native';
 import { ApolloProvider } from '@apollo/client';
+import Constants from 'expo-constants';
 
 import Main from './src/components/Main';
 import createApolloClient from './src/utils/apolloClient';
@@ -8,6 +9,8 @@ import createApolloClient from './src/utils/apolloClient';
 const apolloClient = createApolloClient();
 
 const App = () => {
+  console.log('ENV: ', Constants.manifest.extra.APOLLO_URI);
+
   return (
     <NativeRouter>
         <ApolloProvider client={apolloClient}>
