b6f169d6e54d2cca703859f057048e599846c3b0
Jouni Airaksinen <airaksinen.jouni@gmail.com>
2021-04-19 11:32:50 +0300

    osa 10.23

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -3,7 +3,17 @@ import { FlatList, View, StyleSheet, Text } from 'react-native';
 import RepositoryItem from './RepositoryItem';
 import useRepositories from '../hooks/useRepositories';
 
-export const RepositoryListContainer = ({ repositories }) => {
+import {Picker} from '@react-native-picker/picker';
+
+export const RepositoryListContainer = ({ repositories, onChange }) => {
+   const [selectedLanguage, setSelectedLanguage] = useState("latest");
+
+   //onChange(selectedLanguage);
+   useEffect(() => {
+    console.log('# Effect: ', selectedLanguage)
+         onChange(selectedLanguage)
+     }, [selectedLanguage])
+
   const repositoryNodes = repositories
     ? repositories.edges.map((edge) => edge.node)
     : [];
@@ -13,15 +23,31 @@ export const RepositoryListContainer = ({ repositories }) => {
      data={repositoryNodes}
      keyExtractor={({ id }) => id}
      renderItem={({ item }) => <RepositoryItem repository={item} />}
-     //renderItem={renderItem}
+     ListHeaderComponent={() =>
+     <Picker
+       selectedValue={selectedLanguage}
+        style={{ height: 50 }}
+       onValueChange={(itemValue, itemIndex) =>
+         setSelectedLanguage(itemValue)
+       }>
+       <Picker.Item label="Latest repositories" value="latest" />
+       <Picker.Item label="Highest rated repositories" value="highest" />
+       <Picker.Item label="Lowest rated repositories" value="lowest" />
+     </Picker>
+     }
     />
   );
 };
 
 const RepositoryList = () => {
-  const { repositories } = useRepositories();
+  const [value, setValue] = useState("latest");
+  const { repositories } = useRepositories({value});
+console.log('# Order: ', value)
+  const getOrder = ({order}) => {
+    console.log('#get Order: ', order)
+  }
 
-  return <RepositoryListContainer repositories={repositories} />;
+  return <RepositoryListContainer onChange={setValue} repositories={repositories} />;
 };
 
 export default RepositoryList;
--- src/hooks/useRepositories.js
+++ src/hooks/useRepositories.js
@@ -2,20 +2,53 @@ import { useQuery } from '@apollo/client';
 
 import { GET_REPOSITORIES } from '../graphql/queries';
 
-const useRepositories = () => {
-  const { data, error, loading, refetch } = useQuery(GET_REPOSITORIES, {
-    fetchPolicy: 'cache-and-network',
-  });
-  let repositories;
-  // console.log('## REP query:: -loading: ', loading, ' -data: ', data)
+const queryFn = ({ orderBy, orderDirection }) => {
+    console.log('## Repository queryFN: ', orderBy, ' :: ', orderDirection)
+   const { data, error, loading, refetch } = useQuery(GET_REPOSITORIES, {
+        fetchPolicy: 'cache-and-network',
+        variables: {
+             orderBy,
+             orderDirection
+         }
+    });
 
-    if (error) return `Error! ${error}`;
+    return { data, error, loading, refetch }
+}
 
-    if (data) {
-        repositories = data.repositories
+const useRepositories = ({value}) => {
+
+  let repositories, orderBy, orderDirection;
+
+    if (value==="latest") {
+          orderBy="CREATED_AT", orderDirection="DESC"
+          const { data, error, loading, refetch } = queryFn({orderBy, orderDirection})
+      if (error) return `Error! ${error}`;
+      if (data) { repositories = data.repositories }
+
+     return { repositories, loading, refetch };
     }
 
-  return { repositories, loading, refetch };
+    if (value==="highest") {
+          orderBy="RATING_AVERAGE", orderDirection="DESC"
+          const { data, error, loading, refetch } = queryFn({orderBy, orderDirection})
+
+          if (error) return `Error! ${error}`;
+          if (data) { repositories = data.repositories }
+
+         return { repositories, loading, refetch };
+    }
+
+    if (value==="lowest") {
+          orderBy="RATING_AVERAGE", orderDirection="ASC"
+          const { data, error, loading, refetch } = queryFn({orderBy, orderDirection})
+
+          if (error) return `Error! ${error}`;
+          if (data) { repositories = data.repositories }
+
+          return { repositories, loading, refetch };
+    }
+
+  return { repositories };
 };
 
 export default useRepositories;
